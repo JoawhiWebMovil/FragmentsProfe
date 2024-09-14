b4b3f89404deac38c4def85367a9df05b4fb1d2c
Luis Chang <luis.chang@outlook.com>
2024-09-14 12:33:14 -0500

    ItemMusica en MusicaFragment

---
+++ app/src/main/java/dev/luischang/fragments/ui/model/SongModel.kt
@@ -0,0 +1,9 @@
+package dev.luischang.fragments.ui.model
+
+data class SongModel(
+    val id: Int,
+    val songName: String,
+    val album: String,
+    val countPlay: String,
+    val songTime: String
+)
