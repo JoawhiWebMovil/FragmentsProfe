e73d2f0fd740e5a46c2debc5339a2805416811cc
Luis Chang <luis.chang@outlook.com>
2024-09-21 10:52:31 -0500

    Finished Musica Fragment

--- app/src/main/java/dev/luischang/fragments/ui/fragments/MusicaFragment.kt
+++ app/src/main/java/dev/luischang/fragments/ui/fragments/MusicaFragment.kt
@@ -5,7 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import dev.luischang.fragments.R
+import dev.luischang.fragments.ui.adapter.SongAdapter
+import dev.luischang.fragments.ui.model.SongModel
 
 
 class MusicaFragment : Fragment() {
@@ -15,6 +19,38 @@ class MusicaFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_musica, container, false)
+        val view: View =  inflater.inflate(R.layout.fragment_musica, container, false)
+
+        val rvMusica: RecyclerView = view.findViewById(R.id.rvMusica)
+        rvMusica.layoutManager = LinearLayoutManager(requireContext())
+        rvMusica.adapter = SongAdapter(getSongs())
+        return view
+
+    }
+
+    private fun getSongs(): List<SongModel>{
+        val lstSong: ArrayList<SongModel> = ArrayList()
+
+        lstSong.add(
+            SongModel(1, R.drawable.intheend, "In the end"
+                        , "Hybrid Theory", "100.000"
+                    , "4:23"))
+        lstSong.add(
+            SongModel(2, R.drawable.castleofglass, "Castle of Glass"
+                , "Hybrid Theory", "200.000"
+                , "3:58"))
+        lstSong.add(
+            SongModel(3, R.drawable.numb, "Numb"
+                , "Hybrid Theory", "100.000"
+                , "4:23"))
+        lstSong.add(
+            SongModel(4, R.drawable.onestepcloser, "One step closer"
+                , "Hybrid Theory", "98.000"
+                , "5:18"))
+        lstSong.add(
+            SongModel(5, R.drawable.whativedone, "What I've done"
+                , "Hybrid Theory", "222.000"
+                , "4:10"))
+        return lstSong
     }
 }
--- app/src/main/java/dev/luischang/fragments/ui/model/SongModel.kt
+++ app/src/main/java/dev/luischang/fragments/ui/model/SongModel.kt
@@ -2,6 +2,7 @@ package dev.luischang.fragments.ui.model
 
 data class SongModel(
     val id: Int,
+    val image: Int,
     val songName: String,
     val album: String,
     val countPlay: String,
